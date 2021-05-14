e7b4f9ffe9312e3d57a5b39074347102092ba6f0
Aavash <aavashsthapit@gmail.com>
2021-05-14 15:56:08 -0500

    Testing if correct image is displayed

--- app/src/androidTest/java/com/aavashsthapit/myapplication/ui/fragments/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/aavashsthapit/myapplication/ui/fragments/HomeFragmentTest.kt
@@ -1,25 +1,17 @@
 package com.aavashsthapit.myapplication.ui.fragments
 
-import android.view.View
-import androidx.fragment.app.testing.FragmentScenario
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.aavashsthapit.myapplication.R
 import com.aavashsthapit.myapplication.adapters.BaseStreamersAdapter
 import com.aavashsthapit.myapplication.adapters.StreamersAdapter
-import com.aavashsthapit.myapplication.data.entity.Streamer
 import com.aavashsthapit.myapplication.data.repo.FakeRepo
 import com.aavashsthapit.myapplication.databinding.FragmentHomeBinding
 import com.aavashsthapit.myapplication.launchFragmentInHiltContainer
@@ -29,10 +21,12 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import javax.inject.Inject
 import org.mockito.Mockito.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import com.aavashsthapit.myapplication.ui.viewmodels.MainViewModel
+import com.google.common.truth.Truth.assertThat
+
 @MediumTest
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
@@ -42,36 +36,37 @@ class HomeFragmentTest {
     var hiltRule = HiltAndroidRule(this)
 
     @Inject
-    lateinit var streamersAdapter : StreamersAdapter
+    lateinit var streamersAdapterForPractice : StreamersAdapter
 
     @Before
     fun setup(){
         hiltRule.inject()
+        streamersAdapterForPractice.streamers = FakeRepo.testStreamers
     }
 
     @Test
-    fun isAppNamePossible() {
+    fun isAppNameVisible() {
         launchFragmentInHiltContainer<HomeFragment> {
         }
         onView(withId(R.id.app_name)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun isSearchViewPossible() {
+    fun isSearchViewVisible() {
         launchFragmentInHiltContainer<HomeFragment> {
         }
         onView(withId(R.id.sv_search_streamers)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun isCategoryNamePossible() {
+    fun isCategoryNameVisible() {
         launchFragmentInHiltContainer<HomeFragment> {
         }
         onView(withId(R.id.tv_category)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun isRecyclerViewPossible() {
+    fun isRecyclerViewVisible() {
         launchFragmentInHiltContainer<HomeFragment> {
             streamersAdapter.apply {
                 streamers = FakeRepo.testStreamers
@@ -99,8 +94,13 @@ class HomeFragmentTest {
                 layoutManager = LinearLayoutManager(requireContext()) //requireContext error here?
             }
         }
+
         onView(withId(R.id.rv_all_streamers))
-                .perform(RecyclerViewActions.scrollToPosition<BaseStreamersAdapter.StreamerViewHolder>(0))
+                .perform(
+                        RecyclerViewActions.scrollToPosition<BaseStreamersAdapter.StreamerViewHolder>(
+                                0
+                        )
+                )
     }
 
     @Test
@@ -122,13 +122,22 @@ class HomeFragmentTest {
                 adapter = streamersAdapter
                 layoutManager = LinearLayoutManager(requireContext()) //requireContext error here?
             }
+
             //Setting mock nav controller
-            Navigation.setViewNavController(requireView(), navController)
+            Navigation.setViewNavController(
+                    requireView(),
+                    navController
+            )
         }
 
         //Mock click on recyclerView
         onView(withId(R.id.rv_all_streamers))
-                .perform(RecyclerViewActions.actionOnItemAtPosition<BaseStreamersAdapter.StreamerViewHolder>(2, click()))
+                .perform(
+                        RecyclerViewActions.actionOnItemAtPosition<BaseStreamersAdapter.StreamerViewHolder>(
+                                2,
+                                click()
+                        )
+                )
 
         //Verify that we navigated to the correct place
         verify(navController).navigate(
@@ -136,7 +145,30 @@ class HomeFragmentTest {
         )
     }
 
+    @Test
+    fun correctImageDisplayedInView() {
+        val imageUrl = "https://specials-images.forbesimg.com/imageserve/5f5f55887d9eec237a586841/960x0.jpg?fit=scale"
+        val testViewModel = MainViewModel(FakeRepo)
+        launchFragmentInHiltContainer<HomeFragment> {
+            streamersAdapter.apply {
+                streamers = FakeRepo.streamers
+                mainViewModel = testViewModel
+            }
+            //Instantiating binding
+            binding = FragmentHomeBinding.bind(requireView()) //viewBinding
+            binding.rvAllStreamers.apply {
+                adapter = streamersAdapter
+                layoutManager = LinearLayoutManager(requireContext()) //requireContext error here?
+            }
+        }
 
+        assertThat(
+                streamersAdapterForPractice.streamers[0].thumbnail_url
+//                testViewModel.streamers.value?.data?.get(0)?.thumbnail_url
+        ).contains(
+                imageUrl
+        )
+    }
 
 
 }
