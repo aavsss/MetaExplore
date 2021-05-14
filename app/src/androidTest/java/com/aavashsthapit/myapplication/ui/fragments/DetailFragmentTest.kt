15cd04776ee9893a1b71e84ec27ded63c6b6c371
Aavash <aavashsthapit@gmail.com>
2021-05-14 10:48:31 -0500

    Detail Fragment Tested

--- app/src/androidTest/java/com/aavashsthapit/myapplication/ui/fragments/DetailFragmentTest.kt
+++ app/src/androidTest/java/com/aavashsthapit/myapplication/ui/fragments/DetailFragmentTest.kt
@@ -1,11 +1,18 @@
 package com.aavashsthapit.myapplication.ui.fragments
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.aavashsthapit.myapplication.R
+import com.aavashsthapit.myapplication.launchFragmentInHiltContainer
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 @MediumTest
 @HiltAndroidTest
@@ -16,7 +23,26 @@ class DetailFragmentTest {
     var hiltRule = HiltAndroidRule(this)
 
     @Before
-    fun setup(){
+    fun setup() {
         hiltRule.inject()
+        launchFragmentInHiltContainer<DetailFragment> {
+        }
     }
+
+    @Test
+    fun isNameVisible() {
+        onView(withId(R.id.tv_name)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun isImageViewVisible() {
+        onView(withId(R.id.iv_streamer_img)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun isCategoryVisible() {
+        onView(withId(R.id.tv_category)).check(matches(isDisplayed()))
+    }
+
+
 }
